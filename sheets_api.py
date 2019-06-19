e640b7db169237444d787a2f6c11189e0ed49e4e
Sviatoslav Pereverziev <sviatoslav.pereverziev@gmail.com>
2019-06-19 21:19:56 +0300

    Added a function to form a new table

--- sheets_api.py
+++ sheets_api.py
@@ -2,12 +2,16 @@
 from configparser import ConfigParser
 import os
 import httplib2
+import logging
 import apiclient.discovery
 from oauth2client.service_account import ServiceAccountCredentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from pprint import pprint
 import pickle
+from datetime import datetime
+from database import DB
+from cryptography.fernet import Fernet
 
 
 class SheetsApi:
@@ -15,10 +19,18 @@ class SheetsApi:
         self.scope = ['https://www.googleapis.com/auth/drive']
         self.credentials_file = None
         self.service = None
+
+        self.model_spreadsheet_id = None
+        self.model_sheets_id = None
+
         self.config = ConfigParser()
         self.set_settings()
 
     def set_settings(self):
+        self.config.read(os.path.dirname(os.path.abspath(__file__)) + '/app.ini')
+        self.model_spreadsheet_id = self.config.get('SHEETS_API', 'model_spreadsheet_id')
+        self.model_sheets_id = [int(sheets_id) for sheets_id in
+                                self.config.get('SHEETS_API', 'model_sheets_id').split(',')]
         pass
 
     def set_service(self):
@@ -45,82 +57,149 @@ class SheetsApi:
         self.service = apiclient.discovery.build('sheets', 'v4', credentials=creds)
         pprint(self.service)
 
-    def create_sheet(self):
-
-        # create spreadsheet
-        print('create spreadsheet')
-        request_body = {
-            'properties': {
-                'title': 'Саша сучка а Славик красавчик',
-                'locale': 'ru_RU',
-                'timeZone': 'Europe/Helsinki'
+    def create_sheet(self, db):
+        users_id = db.create_sheets_for()
+        for user_id in users_id:
+            request_body = {
+                'properties': {
+                    'title': self.encrypt(user_id[0]),
+                    'locale': 'uk',
+                    'timeZone': 'Europe/Kiev'
+                }
             }
-        }
-        request = self.service.spreadsheets().create(body=request_body)
-        response = request.execute()
-        new_spreadsheet_id = response['spreadsheetId']
-        spreadsheet_url = response['spreadsheetUrl']
-        pprint(response)
+            try:
+                request = self.service.spreadsheets().create(body=request_body)
+                response = request.execute()
+            except Exception as e:
+                logging.error(f'Error create spreadsheet for user {user_id[0]}. Error: {e}')
+                continue
 
-        # copy to spreadsheet
-        print('copy to spreadsheet')
-        model_spreadsheet_id = '1IxC2KFWu-ywL7K4k_db9gl7bIpIphiUXbVcJguhrbG8'
-        request_body = {
-            'destinationSpreadsheetId': new_spreadsheet_id
-        }
-        sheets_id = [1446449488, 2102683703, 866833467, 1215147421]
-        sheets_name = {}
-        for sheet_id in sheets_id:
-            request = self.service.spreadsheets().sheets().copyTo(spreadsheetId=model_spreadsheet_id, sheetId=sheet_id,
-                                                                  body=request_body)
+            new_spreadsheet_id = response['spreadsheetId']
+            spreadsheet_url = response['spreadsheetUrl']
+
+            # copy to spreadsheet
+            request_body = {
+                'destinationSpreadsheetId': new_spreadsheet_id
+            }
+            sheets_name = {}
+            error_copy_to_spreadsheet = None
+            for sheet_id in self.model_sheets_id:
+                try:
+                    request = self.service.spreadsheets().sheets().copyTo(spreadsheetId=self.model_spreadsheet_id,
+                                                                          sheetId=sheet_id,
+                                                                          body=request_body)
+                    response = request.execute()
+                except Exception as e:
+                    logging.error(f'Error at copy sheets {sheet_id} for user {user_id[0]}. Error: {e}')
+                    error_copy_to_spreadsheet = True
+                    break
+                else:
+                    sheets_name.update({response['sheetId']: response['title'].replace('Копія аркуша ', '')})
+
+            if error_copy_to_spreadsheet:
+                continue
+
+            # rename sheets
+            request_body = {
+                'requests': [
+                ]
+            }
+            for sheet_id, sheet_name in sheets_name.items():
+                request_body['requests'].append({
+                    'updateSheetProperties': {
+                        'properties': {
+                            'sheetId': sheet_id,
+                            'title': sheet_name,
+                            'gridProperties': {
+                                'columnCount': 26,
+                                'rowCount': 1000
+                            }
+                        },
+                        'fields': '*'
+                    },
+                })
+            request_body['requests'].append({
+                'deleteSheet': {
+                    'sheetId': 0
+                }
+            })
+            try:
+                self.service.spreadsheets().batchUpdate(spreadsheetId=new_spreadsheet_id,
+                                                        body=request_body).execute()
+            except Exception as e:
+                logging.error(f'Error at rename sheets and delete sheet1 for user {user_id[0]}. Error: {e}')
+
+            # open permission
+            try:
+                self.driveService.permissions().create(
+                    fileId=new_spreadsheet_id,
+                    body={'type': 'anyone', 'role': 'writer'},
+                    fields='*'
+                ).execute()
+            except Exception as e:
+                logging.error(f'Error opening access rightsfor user {user_id[0]}. Error: {e}')
+                continue
+
+            if not db.set_google_sheets_id(spreadsheet_url, user_id[0]):
+                logging.error(f'Error set google sheets id for user {user_id[0]}. Error: {e}')
+
+    @staticmethod
+    def encrypt(data):
+        with open(os.path.dirname(os.path.realpath(__file__)) + '/private/key_for_name.txt') as file:
+            key = file.read().encode('utf-8')
+        cipher = Fernet(key)
+        return cipher.encrypt(str(data).encode('utf-8')).decode('utf-8')
+
+    @staticmethod
+    def decrypt(data):
+        with open(os.path.dirname(os.path.realpath(__file__)) + '/private/key_for_name.txt') as file:
+            key = file.read().encode('utf-8')
+        cipher = Fernet(key)
+        return cipher.decrypt(data.encode('utf-8')).decode('utf-8')
+
+    def add_data(self, db):
+        for i in range(1, 101):
+            request_body = {
+                "majorDimension": "ROWS",
+                "range": "Data",
+                "values": [
+                    [
+                        datetime.now().date().strftime('%Y-%m-%d'),
+                        datetime.now().time().strftime('%H:%M:%S'),
+                        20,
+                        'Питание',
+                        None,
+                        34.5 * i,
+                        'Доход'
+                    ]
+                ]
+            }
+            request = self.service.spreadsheets().values().append(
+                spreadsheetId='1x4KvEWVTjONCpBEsleBRGX_EOqhDfiGbDb_aLaOuGO8', range='Data',
+                body=request_body,
+                valueInputOption='USER_ENTERED')
+
             response = request.execute()
-            sheets_name.update({response['sheetId']: response['title'].replace(' (копия)', '')})
+
             pprint(response)
 
-        # rename sheets
-        print('rename sheets and delete sheet1')
-        request_body = {
-            'requests': [
-            ]
-        }
-        # sheets_name = {1321156125: 'Расходы', 1692779675: 'Баланс', 379428490: 'Доходы', 1765887935: 'Data'}
-        for sheet_id, sheet_name in sheets_name.items():
-            # print(sheet_id, sheet_name)
-            request_body['requests'].append({
-                'updateSheetProperties': {
-                    'properties': {
-                        'sheetId': sheet_id,
-                        'title': sheet_name,
-                        'gridProperties': {
-                            'columnCount': 26,
-                            'rowCount': 1000
-                        }
-                    },
-                    'fields': '*'
-                },
-            })
-        request_body['requests'].append({
-            'deleteSheet': {
-                'sheetId': 0
-            }
-        })
-        request = self.service.spreadsheets().batchUpdate(spreadsheetId=new_spreadsheet_id,
-                                                          body=request_body)
-        response = request.execute()
-        pprint(response)
+    def can_connect_sheet(self, db):
+        return True
 
-        # open permission
-        print('open permission')
-        response = self.driveService.permissions().create(
-            fileId=new_spreadsheet_id,
-            body={'type': 'anyone', 'role': 'writer'},  # доступ на чтение кому угодно
-            fields='*'
-        ).execute()
-        pprint(response)
-
-        print(spreadsheet_url)
+    def main(self):
+        db = DB()
+        self.set_service()
+        self.create_sheet(db)
+        # self.can_connect_sheet(db)
+        # self.add_data(db)
 
 
 sheet_api = SheetsApi()
-sheet_api.set_service()
-sheet_api.create_sheet()
+sheet_api.main()
+# x = sheet_api.encrypt(529088251)
+# print(x)
+# print(sheet_api.decrypt(x))
+# sheet_api.set_service()
+# # sheet_api.create_sheet()
+# sheet_api.set_service()
+# sheet_api.add_data('')

50824524a5c0b21449693ff1f1a9c9a33e90cd51
Sviatoslav Pereverziev <sviatoslav.pereverziev@gmail.com>
2019-07-13 14:54:34 +0300

    I wrote a start for two parallel processes: budget_bot and sheets_api, but for now they will be start separately. Removed excess.

--- index.py
+++ index.py
@@ -1,24 +1,24 @@
 # -*- coding: utf-8 -*-
+import logging
+from threading import Thread
+
 from budget_bot import BudgetBot
+from sheets_api import SheetsApi
 
-# дописать главную функцию с запуском парралельных процессов
-# дописать start и help
-# дописать комментарии если нужны
-# удалить лишнее
 # написать файл README
+# написать обработку исключений и логонизацию так, чтоб бот перезапускался
 
+sheet_api = SheetsApi()
 bot = BudgetBot()
 
 
 @bot.message_handler(commands=['add'])
 def add(message):
-    print('add')
     bot.add(message)
 
 
 @bot.message_handler(commands=['settings'])
 def settings(message):
-    print('settings')
     bot.settings(message)
 
 
@@ -39,19 +39,27 @@ def help_(message):
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_inline(call):
-    print('callback_inline')
     bot.callback_inline(call)
 
 
 @bot.message_handler(content_types=['text'])
 def text(message):
-    print('text')
     bot.text(message)
 
 
 def main():
+    logging.error('Run Budget Bot')
     bot.polling()
 
 
 if __name__ == '__main__':
     main()
+    # run_func = [sheet_api.main, main]
+    # run_processes = []
+    # for func in run_func:
+    #     p = Thread(target=func)
+    #     p.start()
+    #     run_processes.append(p)
+    #
+    # for p in run_processes:
+    #     p.join()

f2b3f9f342ff2eaa13017d094ffeb5c95ef60298
Sviatoslav Pereverziev <sviatoslav.pereverziev@gmail.com>
2019-11-02 16:04:52 +0200

    Corrected monobank api client and added currency method.

--- monobank_api.py
+++ monobank_api.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
-import requests
 import json
 import logging
 import requests
 
 
-def get_balance(user_token):
+def get_user_info(user_token):
     header = {'X-Token': user_token, 'accept': 'application/json', 'Content-Type': 'application/json', }
     response = requests.get('https://api.monobank.ua//personal/client-info', headers=header)
     if not response:
@@ -17,22 +16,12 @@ def get_balance(user_token):
     return json.loads(response.content, encoding='utf-8')
 
 
-def get_webhook(user_token):
+def get_cost_statement(user_token, time_from, time_to=None, account=0):
     header = {'X-Token': user_token, 'accept': 'application/json', 'Content-Type': 'application/json', }
-    response = requests.get('https://api.monobank.ua//personal/client-info', headers=header)
-
-    if not response:
-        logging.error('Error get card balance for monobank. Last token symbol: %s' % user_token[-10:])
-        if response.content:
-            logging.error(response.content.decode('utf-8'))
-        return
-
-    return json.loads(response.content, encoding='utf-8')
-
-
-def get_cost_statement(user_token, time_from, time_to=None):
-    header = {'X-Token': user_token, 'accept': 'application/json', 'Content-Type': 'application/json', }
-    response = requests.get('https://api.monobank.ua//personal/statement/0/%s/' % time_from, headers=header)
+    url = 'https://api.monobank.ua//personal/statement/%s/%s/' % (account, time_from)
+    if time_to:
+        url += str(time_to)
+    response = requests.get(url, headers=header)
     if not response:
         logging.error('Error get card balance for monobank. Last token symbol: %s' % user_token[-10:])
         return
@@ -49,4 +38,16 @@ def set_webhook(user_token, url):
         logging.error('Error set webhook for monobank cards. Last token symbol: %s' % user_token[-10:])
         return
 
+    return json.loads(response.content, encoding='utf-8')
+
+
+def get_webhook(user_token):
+    user_info = get_user_info(user_token)
+    if user_info:
+        return user_info.get('webHookUrl')
+
+
+def get_currency():
+    header = {'accept': 'application/json', 'Content-Type': 'application/json', }
+    response = requests.get('https://api.monobank.ua/bank/currency')
     return json.loads(response.content, encoding='utf-8')
